068ab8c9978f187a77b8502f01455e7bee223f37
Joshua Miles <61899128+JoshuaMilesSD@users.noreply.github.com>
2024-06-09 22:33:04 -0700

    Update main.js
    step 94

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-  return " " + character.repeat(rowNumber) + " ";
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i + 1, count));
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+/*while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}*/
+
+for (let i = count; i > false; i = i -= 1) {
+ false + i ;
 }
 
 let result = ""
